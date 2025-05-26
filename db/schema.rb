b4a7bda61496cf481144cfe3c85d5fa6f0e6423f
Carlijn Driessen <carlijndriessen.dev@gmail.com>
2025-05-26 16:26:24 +0200

    associations and validations created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_26_134600) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_141728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_134600) do
     t.string "category"
     t.text "description"
     t.float "price"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
