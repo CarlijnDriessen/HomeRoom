6b62be58fd7a0160b0d3d2ad2585c80e8e046509
Mrinal Verma <mrinalverma1993@gmail.com>
2025-05-29 15:16:12 +0200

    Fixed login redirecting to random listing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :photo])
   end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
