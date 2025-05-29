83a5b1573941cf1f775ab73a134cd3d13f680983
Thomas Bruins <thomas.bruins12@gmail.com>
2025-05-29 17:36:33 +0200

    I made a message function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_28_134043) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_29_133606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_28_134043) do
     t.bigint "listing_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "host_response"
     t.index ["listing_id"], name: "index_bookings_on_listing_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
